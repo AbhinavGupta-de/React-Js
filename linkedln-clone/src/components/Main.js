44111f5f6ba6d26422610e207b96c4ddfde4a04e
Abhinav Gupta <abhinav.gupta@outlook.de>
2022-07-01 09:35:18 +0530

    making static post

--- linkedln-clone/src/components/Main.js
+++ linkedln-clone/src/components/Main.js
@@ -1,3 +1,9 @@
+/*  
+
+Hello, I am Abhinav Gupta. Thanks for visiting the project made up me, hope you like and if you find that you tell me to imporove this do tell me. How to reach me? Mail me at abhinavgupta4505@gmail.com
+
+*/
+
 import styled from 'styled-components';
 
 const Main = (props) => {
@@ -31,6 +37,58 @@ const Main = (props) => {
 					</button>
 				</div>
 			</ShareBox>
+			<div>
+				<Article>
+					<SharedActor>
+						<a>
+							<img src="/images/user.svg" alt="" />
+							<div>
+								<span>Tittle</span>
+								<span>Info</span>
+								<span>Date</span>
+							</div>
+						</a>
+						<button>
+							<img src="/images/ellipsis-icon.svg" alt="" />
+						</button>
+					</SharedActor>
+					<Discription>Discription</Discription>
+					<SharedImg>
+						<a>
+							<img src="/images/shared-image.webp" alt="" />
+						</a>
+					</SharedImg>
+					<SocialCount>
+						<li>
+							<button>
+								<img src="/images/like-emoji.png" alt="" />
+								<img src="/images/celebrate-icon.png" alt="" />
+								<span>75</span>
+							</button>
+						</li>
+						<li>
+							<a>2 comments</a>
+						</li>
+					</SocialCount>
+					<button>
+						<img src="/images/like-icon.svg" alt="" />
+						<span>Like</span>
+					</button>
+					<button>
+						<img src="/images/comment-icon.svg" alt="" />
+						<span>Comment</span>
+					</button>
+					<button>
+						<img src="/images/share-icon.svg" alt="" />
+						<span>Share</span>
+					</button>
+					<button>
+						{/* Add the svg code to the file first */}
+						<img src="/images/send-icon.svg" alt="" />
+						<span>Send</span>
+					</button>
+				</Article>
+			</div>
 		</Container>
 	);
 };
@@ -109,6 +167,124 @@ const ShareBox = styled(CommonCard)`
 					width: 22px;
 				}
 			}
+		}
+	}
+`;
+
+const Article = styled(CommonCard)`
+	padding: 0px;
+	margin: 0 0 8px;
+	overflow: visible;
+`;
+
+const SharedActor = styled.div`
+	padding-right: 40px;
+	flex-wrap: nowrap;
+	padding: 12px 16px 0;
+	margin-bottom: 8px;
+	align-items: center;
+
+	display: flex;
+
+	a {
+		margin-right: 12px;
+		flex-grow: 1;
+		overflow: hidden;
+		display: flex;
+
+		img {
+			width: 48px;
+			height: 48px;
+		}
+
+		& > div {
+			display: flex;
+			flex-direction: column;
+			flex-grow: 1;
+			flex-basis: 0;
+			overflow: hidden;
+			margin-left: 8px;
+
+			span {
+				text-align: left;
+				&:first-child {
+					font-size: 14px;
+					font-weight: 700;
+					color: rgba(0, 0, 0, 1);
+				}
+
+				&:nth-child(n + 1) {
+					font-size: 12px;
+					color: black;
+				}
+			}
+		}
+	}
+
+	button {
+		background: white;
+		outline: none;
+		border: none;
+		position: absolute;
+		right: 12px;
+		top: 0;
+	}
+`;
+
+const Discription = styled.div`
+	padding: 0 16px;
+	overflow: hidden;
+	color: rgba(0, 0, 0, 0.9);
+	font-size: 14px;
+	text-align: left;
+`;
+
+const SharedImg = styled.div`
+	margin-top: 8px;
+	display: block;
+	width: 100%;
+	position: relative;
+	background-color: #f9fafb;
+
+	img {
+		width: 98%;
+		height: 100%;
+	}
+`;
+
+const SocialCount = styled.ul`
+	line-height: 1.5;
+	display: flex;
+	align-items: flex-start;
+	overflow: auto;
+	border-bottom: 1px solid #e9e5df;
+	list-style: none;
+	margin: 0 14px;
+	padding: 2px 0;
+	justify-content: space-between;
+
+	li {
+		margin-right: 5px;
+		font-size: 12px;
+		button {
+			display: flex;
+			align-items: center;
+			border: none;
+			background-color: white;
+
+			img {
+				height: 12px;
+				width: 12px;
+				margin-right: 2px;
+			}
+
+			span {
+				color: black;
+				margin-left: 2px;
+			}
+		}
+		a {
+			color: black;
 		}
 	}
 `;
